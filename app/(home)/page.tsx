bf4b700423c698ea3c4aeca861dd87ec35d73ca5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-08-07 18:52:36 +0530

    nextjsportfolio

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,7 @@ function page() {
   return (
     <div className='min-h-screen bg-black overflow-hidden'>
       {/* overflow-hidden */}
-      <div className=' dark:bg-black bg-white  dark:bg-grid-white/[0.09] bg-grid-black/[0.2] relative'>
+      <div className=' dark:bg-black bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2] relative'>
       <div  className=' max-w-7xl mx-auto p-5'>
       <Navbar />
       < Herosection />
@@ -17,13 +17,13 @@ function page() {
       <div className='h-10 x1:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 x1:bottom-0 w-full"'>
 
       </div>
-     
-      </div>
       <div className=' max-w-7xl mx-auto p-5 mt-20'>
       <Skills/>
       <Projects />
       <Footer />
       </div>
+      </div>
+      
     </div>
   )
 }
