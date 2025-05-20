c606038677e4f440e220db089a20ed89d9d2275f
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-05-21 00:22:03 +0530

    img

--- app/(home)/components/Herosection.tsx
+++ app/(home)/components/Herosection.tsx
@@ -12,7 +12,7 @@ function Herosection() {
         <h1 className="text-4xl lg:text-7xl font-bold text-white-500">
           Hello👋,Nice to meet you!
           <br />
-          <div className="-ml-14 -rotate-1">
+          <div className="xl:-ml-14 lg:-ml-14 xl-rotate-1 lg-rotate-1 sm:-ml-14 mt-4 xl:mt-0 lg:mt-0">
             <FuzzyText
               baseIntensity={0.2}
               hoverIntensity={0.5}
@@ -29,7 +29,8 @@ function Herosection() {
           }
         </p>
         <Link
-          href={" mailto:kartikmehta650@gmail.com"}
+          href={" https://drive.google.com/drive/folders/1untiORf7J_aWXV_ETxj0-4-l_WTU0MbE"}
+          // href={" mailto:kartikmehta650@gmail.com"}
           className="inline-block"
         >
           <div>
